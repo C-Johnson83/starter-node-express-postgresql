e565c8c7665577715c203f7d2296aa3f186a9b1f
chris <chrisj21283@gmail.com>
2024-06-12 18:37:59 -0400

    aggregate knex

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -21,7 +21,21 @@ function read(req, res) {
   const { product: data } = res.locals;
   res.json({ data });
 }
+
+async function listOutOfStockCount(req, res, next) {
+  res.json({ data: await productsService.listOutOfStockCount() });
+}
+async function listPriceSummary(req, res, next) {
+  res.json({ data: await productsService.listPriceSummary() });
+}
+async function listTotalWeightByProduct(req, res) {
+  res.json({ data: await productsService.listTotalWeightByProduct() });
+}
+
 module.exports = {
   read: [asyncErrorBoundary(productExists), read],
   list: asyncErrorBoundary(list),
+  listOutOfStockCount: asyncErrorBoundary(listOutOfStockCount),
+  listPriceSummary: asyncErrorBoundary(listPriceSummary),
+  listTotalWeightByProduct: asyncErrorBoundary(listTotalWeightByProduct),
 };
